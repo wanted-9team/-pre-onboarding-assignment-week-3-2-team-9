e6a437865c9ba268a8fa7ed4d55f3e3bf5324b5a
chaejung Kim <whenucan35@gmail.com>
2022-09-19 03:25:14 +0900

    [FIX] url 설정
    - 배포 url 변경

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:4000'
+axios.defaults.baseURL = 'http://pre-onboarding-09-sparkpet.herokuapp.com'
 
 export const pageOptions = {
   pageLimit: 4,
